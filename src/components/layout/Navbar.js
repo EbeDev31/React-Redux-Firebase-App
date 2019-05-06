86041bc5975bed35acc8ad6e44dae49ffd0a29ad
Ebe <ebedev31@gmail.com>
2019-05-06 20:42:40 +0200

    setup for user Login

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {Link} from "react-router-dom"
 import SignedInLink from "./SignedInLink"
 import SignedOutLink from "./SignedOutLink"
+import {connect} from "react-redux"
 
-const Navbar =()=>{
-
+const Navbar =(props)=>{
+    const { auth } = props;
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
@@ -18,4 +19,11 @@ const Navbar =()=>{
     )
 }
 
-export default  Navbar
+const mapStateToProps = (state) => {
+    // console.log(state);
+    return{
+      auth: state.firebase.auth
+    }
+  }
+
+export default  connect(mapStateToProps)(Navbar)
