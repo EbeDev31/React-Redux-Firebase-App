20bacc746ebbd20e0cf69e288f004ee108c046f7
ebedev31 <ebedev31@gmail.com>
2019-06-19 22:38:43 +0200

    aesthetics for delete project button

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import moment from "moment"
+import moment from "moment";
+import { Link } from "react-router-dom"
 
-const ProjectSummary =({project})=> {
-  return (
-      
+
+const ProjectSummary = ({ project }) => {
+    return (
+
         <div className="card z-depth=0 project-summary">
-            <div className="card z-depth=0 project-summary">
-                <span className="card-title">{project.title}</span>    
-                <p>Posted By {project.authorFirstName} {project.authorLastName}</p>
-                <p className="grey-text">Created {moment(project.createdAt.toDate()).calendar()}</p>
+            <div className="proj-card">
+                <div className="card z-depth=0 proj-details">
+                    <Link to={'/project/' + project.id} key={project.id}>
+                        <span className="card-title">{project.title}</span>
+                        <p>Posted By {project.authorFirstName} {project.authorLastName}</p>
+                        <p className="grey-text">Created {moment(project.createdAt.toDate()).calendar()}</p>
+                    </Link>
+                </div>
+
+
+                <div className="proj-delete">
+                    <p>
+                        <i class="medium material-icons"
+                            onClick={() => { alert("On it!!") }}
+                        >delete</i>
+                    </p>
+                </div>
             </div>
+
         </div>
     )
 }
